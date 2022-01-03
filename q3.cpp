01a44f8aa31c8bb97446b687c0534a281be26cd8
maitri-vv <87691594+maitri-vv@users.noreply.github.com>
2022-01-03 11:22:40 +0530

    Create q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        int ans=0;
+        long long p=1;
+        for(int st=0,ed=0;ed<n;ed++){
+            p*=a[ed];
+            while(st<ed && p >=k){
+                p/=a[st++];
+            }
+            if(p<k){
+                int len=ed-st+1;
+                ans+=len;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

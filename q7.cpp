423ee03d631c3fba612433266f309d039b534f8e
maitri-vv <87691594+maitri-vv@users.noreply.github.com>
2022-01-03 11:26:17 +0530

    Create q7.cpp

---
+++ q7.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        if((K-1+M%N)%N==0)return N;
+        return (K-1+M%N)%N;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends

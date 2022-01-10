459f4b686c8ba2be2174160b3a36ae6725da80e3
maitri-vv <maitrivaghasiya99@gmail.com>
2022-01-10 19:48:04 +0530

    Create m2aq8.cpp

---
+++ Milestone2 amazon/m2aq8.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        long long dp[m+1];
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=m;i++)
+        dp[i]=min(dp[i-2],dp[i-1])+1;
+        
+        return dp[m];
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
